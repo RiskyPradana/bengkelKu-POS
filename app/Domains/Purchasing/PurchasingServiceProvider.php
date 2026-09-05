7643ed9ff2bab6df8f64fd8d8566ff63bab206fd
Risky Pradana <50886815+RiskyPradana@users.noreply.github.com>
2026-09-05 10:40:20 +0800

    feat: register Purchasing domain service provider

---
+++ app/Domains/Purchasing/PurchasingServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Domains\Purchasing;
+
+use Illuminate\Support\ServiceProvider;
+
+class PurchasingServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+    }
+
+    public function boot(): void
+    {
+    }
+}
